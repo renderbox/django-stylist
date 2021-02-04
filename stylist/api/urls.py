3e9a66e8fd71281192e2448ddeda991500005f11
mgcamisa <mgcamisa@gmail.com>
2021-02-04 14:34:06 -0800

    add duplication feature; don't recompile css if the only change is to the name

--- stylist/api/urls.py
+++ stylist/api/urls.py
@@ -4,4 +4,5 @@ from stylist.api import views
 
 urlpatterns = [
     path('style/create/', views.StyleCreateAPIView.as_view(), name='api-create-style'),
+    path('style/<uuid:uuid>/duplicate/', views.StyleDuplicateAPIView.as_view(), name='api-duplicate-style'),
 ]

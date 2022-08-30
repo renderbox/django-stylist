ac0c8852dc7438318940e1351df64e23cf6ae56f
DeVon Jackson <devonj@whitemoondreams.com>
2022-08-30 10:59:06 +0100

    removes compile styles action

--- stylist/admin.py
+++ stylist/admin.py
@@ -2,18 +2,9 @@ from django.contrib import admin
 
 from .models import Style
 
-
-def compile_styles(modeladmin, request, queryset):
-    for q in queryset:
-        q.compile_attrs()
-compile_styles.short_description = "Compile selected styles"
-
-
 class StyleAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'slug', 'site', 'attrs', 'enabled')
     list_filter = ('site', 'enabled')
     search_fields = ('name', 'slug')
-    actions = [compile_styles]
-
 
 admin.site.register(Style, StyleAdmin)

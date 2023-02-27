01c2e898f1008bdcf96743d93da31db08b886a6b
mackenzie <mgcamisa@gmail.com>
2023-02-27 12:07:36 -0800

    add crispy template pack to settings

--- develop/develop/settings.py
+++ develop/develop/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
